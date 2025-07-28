5caf4d1cf5be6ea6b97d8e4fd471e007b513d0a7
Muhammad Aneeb <aneebkashifpro@gmail.com>
2025-07-28 08:42:29 -0700

    ADDIng if with .env

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-require("dotenv").config();
-
+// Conditionally load dotenv only in development
+if (process.env.NODE_ENV !== 'production') {
+  require("dotenv").config();
+}
 const express = require("express");
 const { connectToMongoDB } = require("./connect");
 const carRoutes = require("./routes/carRoutes");
