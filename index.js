8c9ea01a4694184b7a6b461c3b134fe01a496de3
Muhammad Aneeb <aneebkashifpro@gmail.com>
2025-07-29 02:45:32 -0700

    again

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.use("/api/admin", adminRoutes);
 app.get("/test", (req, res) => {
   res.send("everything is fine");
 });
-app.get("again-test" , (req , res ) =>{
+app.get("/again-test" , (req , res ) =>{
   res.send("everything is again fine");
 })
 connectToMongoDB(process.env.MONGO_URL)
